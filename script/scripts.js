d2b5f762e7a1de63079d99fe554b9cf9e8e99b4f
Sergey Uglik <antihate1377@gmail.com>
2021-04-06 00:42:23 +0300

    Complete second lesson

--- script/scripts.js
+++ script/scripts.js
@@ -1,8 +1,16 @@
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
-alert('Hello, world');
-console.log('Hello, world');
+let money = 1500,
+    income = 'programming',
+    addExpenses = 'girlfriend, sport, food',
+    deposit = true,
+    mission = 100000,
+    period = 12;
+console.log('Type of money: ', typeof(money));
+console.log('Type of income: ', typeof(income));
+console.log('Type of deposit: ', typeof(deposit));
+console.log('Length of addExpenses: ', addExpenses.length);
+console.log('The period is ' + period + ' months');
+console.log('The goal is to earn ' + mission + ' rubles');
+addExpenses.toLowerCase();
+console.log(addExpenses.split(', '));
+let budgetDay = money / 30;
+console.log('The budget of day is ' + budgetDay + ' rubles');
